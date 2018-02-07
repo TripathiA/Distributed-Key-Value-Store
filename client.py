3b635cefcc5cfc60ccacd17b2ab7251418ebcdcc
Aastha Tripathi <tripathiaastha68@gmail.com>
2018-02-07 16:02:24 -0600

    modified server and client

--- client.py
+++ client.py
@@ -1,15 +1,59 @@
-import xmlrpc.client
 import datetime
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
 import sys
+import xmlrpc.client
+import random
 
-port = sys.argv[1]
-proxy = xmlrpc.client.ServerProxy("http://localhost:"+port+"/")
 
-while True:
-	req = input("Request ? ")
-	print(req)
-	ret = proxy.request(req)
-	print(ret)
-# convert the ISO8601 string to a datetime object
-# converted = datetime.datetime.strptime(today.value, "%Y%m%dT%H:%M:%S")
-# print("Today: %s" % converted.strftime("%d.%m.%Y, %H:%M"))
+servers = dict()
+
+def get(key):
+    serv = random.randint(0,len(servers))
+    val = servers.values()[serv].request("get "+str(key))
+    return val
+
+def put_value(key,value):
+    serv = random.randint(0,len(servers))
+    servers.values()[serv].request("put "+str(key)+" "+str(value))
+
+def connect_to_server(port):
+    proxy = xmlrpc.client.ServerProxy("http://localhost:"+port+"/")
+    servers[port] = proxy
+
+def disconnect_server(port):
+    servers.pop(port,None)
+
+
+#parsing commands
+# valid commands
+# - put key value
+# - get key
+# - connect_to_server server_port
+# - disconnect_server server_port
+
+def parse_req(command):
+    words = command.rstrip().split(" ")
+    print(servers)
+    if("put" in words[0]):
+        put_value(words[1],words[2])
+        return "Inserted the value"
+    else:
+        return "Return: "+str(globals()[words[0]](words[1]))
+
+
+if __name__== "__main__":
+    try:
+        port = int(sys.argv[1])
+    except:
+        print("Give appropriate port number")
+        sys.exit(-1)
+    server = SimpleXMLRPCServer(("localhost", port))
+    print("Listening on port "+str(port))
+    server.register_function(get, "get")
+    server.register_function(put_value, "put_value")
+    server.register_function(connect_to_server, "connect_to_server")
+    server.register_function(disconnect_server, "disconnect_server")
+    server.register_function(parse_req, "request")
+    server.serve_forever()
+
--- server.py
+++ server.py
@@ -5,13 +5,20 @@ import sys
 import xmlrpc.client
 
 
+#stabilize
+#try except if not key available
+#try except for server not available 
+
+
 key_value_store = dict()
 servers = dict()
 
-
 def get(key,distribute=True):
-    curr = key_value_store[key][1]
-    ret = key_value_store[key]
+    curr = -1
+    ret = -1
+    if key in key_value_store:
+        curr = key_value_store[key][1]
+        ret = key_value_store[key]
     if distribute:
         for s in servers:
             val = servers[s].get(key,False)
@@ -19,7 +26,7 @@ def get(key,distribute=True):
                 ret = val
                 key_value_store[key] = val
     print("val "+str(key_value_store))
-    return key_value_store[key]
+    return [ret,curr]
 
 def put_value(key,value):
     if(key in key_value_store):
@@ -37,6 +44,22 @@ def disconnect_server(port):
 def today():
     today = datetime.datetime.today()
     return xmlrpc.client.DateTime(today)
+
+def get_kvstore():
+    return key_value_store
+
+def set_kvstore(kvstore):
+    key_value_store = kvstore
+
+def stabilize():
+    for s in servers:
+        kvstore = servers[s].get_kvstore()
+        for key in kvstore:
+            if ((key not in key_value_store) or (key in key_value_store and kvstore[key][1] > key_value_store[key][1])):
+                key_value_store[key] = kvstore[key]
+    for s in servers:
+        servers[s].set_kvstore(key_value_store)
+
 
 #parsing commands
 # valid commands
